a9792564ca65f4cc67d5e91dedc00c654cc40bcc
DBC Apprentice <apprentice@devbootcamp.com>
2012-07-24 12:08:34 -0700

    submit form and bootstrap done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 HnClone::Application.routes.draw do
+  match '/submit' => 'links#new'
+  resources :links
+
+  root :to => "links#index"
+
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +61,3 @@ HnClone::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
